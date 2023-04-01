8389195d2e90e504a9447e4d581e933c3fceb2ba
diselyuti <dima.selyutin.03@gmail.com>
2023-04-01 03:08:10 +0300

    router fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import {createRouter, createWebHistory} from "vue-router";
 import ProfileView from "../views/ProfileView.vue";
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHistory('/0211/'),
 	routes: [
 		{
 			path: '/',
